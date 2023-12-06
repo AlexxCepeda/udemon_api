e0e2bee49dee19b7a0d28cfa5f7843c880a8c2cf
Alexis Cepeda <alexis9932@gmail.com>
2023-12-05 21:59:10 -0600

    upgrading dependencies

---
+++ src/test/java/org/udemon/udemon/UdemonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.udemon.udemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
